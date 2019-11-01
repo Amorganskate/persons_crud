ac21fbe33daabb788a4eada721402a714288aaed
Steve Shipsey <sshipsey@sidearmsports.com>
2019-10-31 22:20:03 -0400

    Removed extra unused imports

--- Persons/Persons/Models/SeedData.cs
+++ Persons/Persons/Models/SeedData.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Persons.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Persons.Data;
 
 namespace Persons.Models
 {
--- Persons/Persons/Program.cs
+++ Persons/Persons/Program.cs
@@ -1,16 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Persons.Controllers;
 using Persons.Data;
-using Persons.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Persons
 {
